10779d180a98a3bb4266941e26c4114a95e9ef9f
Nyx <DarkValvet@protonmail.com>
2023-12-11 20:44:03 +0200

    Added todo list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("com.airbnb.android:lottie:3.4.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
