c4f7147dfb583ae360e1cfa8ffd652bbbd558e04
Mohamed Aymn <mohamedaymn218@gmail.com>
2024-01-08 15:40:34 +0200

    user login logic

--- app/src/main/java/com/example/todoapp/Login.java
+++ app/src/main/java/com/example/todoapp/Login.java
@@ -1,5 +1,6 @@
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +11,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Login extends AppCompatActivity {
     EditText loginUsername, loginPassword;
     Button loginButton;
     TextView signupRedirect;
-    //This is where we get the ids from the layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +32,58 @@ public class Login extends AppCompatActivity {
         loginButton = findViewById(R.id.login_button);
 
 
-        //this code activate the login button to redirect the user to the main page
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(loginUsername.getText().toString().equals("admin")&& loginPassword.getText().toString().equals("admin")){
-                    //correct
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-                    startActivity(intent);
-                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect
-                    Toast.makeText(Login.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
+
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("users")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String userName = document.getString("name");
+                                        if (userName != null && userName.equals(loginUsername.getText().toString())) {
+                                            Intent intent = new Intent(Login.this, MainActivity.class);
+                                            startActivity(intent);
+                                            Toast.makeText(Login.this,"LOGIN SUCESSED",Toast.LENGTH_SHORT).show();
+                                        }else{
+                                            Toast.makeText(Login.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                } else {
+                                    Toast.makeText(Login.this,"something went wrong",Toast.LENGTH_SHORT).show();
+                                }
+
+
+
+
+
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+//                                        Log.d(TAG, document.getId() + " => " + document.getData());
+//                                    }
+//                                } else {
+//                                    Log.w(TAG, "Error getting documents.", task.getException());
+//                                }
+                            }
+                        });
+
+
+
+//                if(loginUsername.getText().toString().equals("admin")&& loginPassword.getText().toString().equals("admin")){
+//                    //correct
+//                    Intent intent = new Intent(Login.this, MainActivity.class);
+//                    startActivity(intent);
+//                    Toast.makeText(Login.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+//                }else
+//                    //incorrect
+//                    Toast.makeText(Login.this,"LOGIN FAILED",Toast.LENGTH_SHORT).show();
             }
         });
 
